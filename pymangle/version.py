ea28d11e332df4a3e9196848f2a046386383a4f9
Jon Loveday <j.loveday@sussex.ac.uk>
2025-05-02 10:51:20 +0100

    Change dtype=f16 to np.longdouble

--- pymangle/version.py
+++ pymangle/version.py
@@ -2,4 +2,5 @@
 # 1) we don't load dependencies by storing it in __init__.py
 # 2) we can import it in setup.py for the same reason
 # 3) we can import it into the module
-__version__ = '0.9.2'
+# Version 0.9.3, Jon Loveday, 2025-05-02: changed ftype='f16' to np.longdouble
+__version__ = '0.9.3'
